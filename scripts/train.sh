995fb31f15b1d78198c2033cbc36bc1036738588
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2025-04-19 23:46:07 +0900

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,6 +3,9 @@ set -e
 
 export PYTHONPATH=$(pwd)
 
+# ✅ wandbのAPIキーを設定（← ここを自分のキーに書き換えてください）
+export WANDB_API_KEY=your_api_key
+
 LOGFILE="./log/train_log_$(date +%Y%m%d_%H%M%S).txt"
 
 CMD="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True TORCH_DISABLE_DYNAMO=1 TORCH_COMPILE=0 TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0,1,2,3 \
@@ -11,7 +14,7 @@ CMD="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True TORCH_DISABLE_DYNAMO=1 TOR
         --master_port=29501 \
         src/train.py \
         --output_dir ./outputs/llava-datikz-lora\
-        --epochs 2 \
+        --epochs 10 \
         --batch_size 4 \
         --run_name llava-datikz-lora_test \
         "
