23bcc4e4cf4f8289174496615a5bc6fd012d58d5
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2025-04-19 23:45:08 +0900

    Update evaluate_captioning.sh

--- scripts/evaluate_captioning.sh
+++ scripts/evaluate_captioning.sh
@@ -2,10 +2,18 @@
 set -e
 
 export PYTHONPATH=$(pwd)
+export TMPDIR=/tmp
 
-LOGFILE="./log/eval_log_$(date +%Y%m%d_%H%M%S).txt"
-MODEL_PATH="llava-hf/llava-1.5-7b-hf"  #"./output/llava-datikz-lora/llava-datikz-full"
-OUTPUT_PATH="./results/debug.json"
+datetime=$(date +%Y%m%d_%H%M%S)
+LOGFILE="./log/eval_log_${datetime}.txt"
+MODEL_PATH="llava-hf/llava-1.5-7b-hf"    #"llava-hf/llava-1.5-7b-hf"  #"./output/llava-datikz-lora/llava-datikz-full"
+#OUTPUT_PATH="./results/eval_%j_${datetime}.json"
+OUTPUT_PATH="./results/AF_testdata.json"
+
+# 依存ライブラリをインストール（ログにも保存）
+echo "🔧 Installing dependencies..." | tee -a "$LOGFILE"
+pip install --user absl-py nltk rouge-score >> "$LOGFILE" 2>&1
+
 
 CMD="CUDA_VISIBLE_DEVICES=0 \
     python3 src/evaluate_captioning.py \
